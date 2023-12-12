2936937a7c49cdd50544142fe077554ecc805acc
le-noir91 <153179146+le-noir91@users.noreply.github.com>
2023-12-12 10:40:53 +0300

    Update calculator.go

--- calculator.go
+++ calculator.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"strings"
 )
 
 func main() {
 	roman := []string{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X", "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX", "XXI", "XXII", "XXIII", "XXIV", "XXV", "XXVI", "XXVII", "XXVIII", "XXIX", "XXX", "XXXI", "XXXII", "XXXIII", "XXXIV", "XXXV", "XXXVI", "XXXVII", "XXXVIII", "XXXIX", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII", "XLVIII", "XLIX", "L", "LI", "LII", "LIII", "LIV", "LV", "LVI", "LVII", "LVIII", "LIX", "LX", "LXI", "LXII", "LXIII", "LXIV", "LXV", "LXVI", "LXVII", "LXVIII", "LXIX", "LXX", "LXXI", "LXXII", "LXXIII", "LXXIV", "LXXV", "LXXVI", "LXXVII", "LXXVIII", "LXXIX", "LXXX", "LXXXI", "LXXXII", "LXXXIII", "LXXXIV", "LXXXV", "LXXXVI", "LXXXVII", "LXXXVIII", "LXXXIX", "XC", "XCI", "XCII", "XCIII", "XCIV", "XCV", "XCVI", "XCVII", "XCVIII", "XCIX", "C"}
-
+	numeral := []string{"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"}
 	var a, s, b string
 	fmt.Scanln(&a, &s, &b)
 
@@ -28,30 +27,35 @@ func main() {
 				fmt.Println("Вывод ошибки, так как в римской системе нет отрицательных чисел.")
 			}
 		case "*":
-			otvet := (indexOf(roman, a) + 1) * (indexOf(roman, b) + 1) - 1
+			otvet := (indexOf(roman, a)+1)*(indexOf(roman, b)+1) - 1
 			fmt.Println(roman[otvet])
 		case "/":
-			otvet := (indexOf(roman, a) + 1) / (indexOf(roman, b) + 1) - 1
+			otvet := (indexOf(roman, a)+1)/(indexOf(roman, b)+1) - 1
 			fmt.Println(roman[otvet])
 		}
-	} else if _, err := strconv.Atoi(a);
-		(err == nil) && _, err := strconv.Atoi(b);
-	ia := err
-	b2 := err == nil {
-		ia, _ := strconv.Atoi(a)
-		ib, _ := strconv.Atoi(b)
-		switch s{
-	case "+":
-		fmt.Println(ia + ib)
-	case "-":
-		fmt.Println(ia - ib)
-	case "*":
-		fmt.Println(ia * ib)
-	case "/":
-		fmt.Println(ia / ib)
-	}
-	} else {
+	} else if contains(numeral, a) && contains(numeral, b) {
+		switch s {
+		case "+":
+			otvet := indexOf(numeral, a) + indexOf(numeral, b)
+			fmt.Println(otvet)
+		case "-":
+			otvet := indexOf(numeral, a) - indexOf(numeral, b)
+			fmt.Println(otvet)
+		case "*":
+			otvet := indexOf(numeral, a) * indexOf(numeral, b)
+			fmt.Println(otvet)
+		case "/":
+			otvet := indexOf(numeral, a) / indexOf(numeral, b)
+			fmt.Println(otvet)
+		}
+	} else if contains(numeral, a) && contains(roman, b) {
 		fmt.Println("Вывод ошибки, так как используются одновременно разные системы счисления.")
+	} else if contains(numeral, b) && contains(roman, a) {
+		fmt.Println("Вывод ошибки, так как используются одновременно разные системы счисления.")
+	} else {
+		var z string
+		fmt.Scan(&z)
+		fmt.Print("Вывод ошибки, так как строка не является математической операцией.")
 	}
 }
 
